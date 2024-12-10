4613499d6712532cb7b0fced3b218ff047895678
Karanamsuneetha <suneethakaranam30@gmail.com>
2024-12-10 23:09:12 +0530

    Add files via upload

---
+++ exercise_3.py
@@ -0,0 +1,6 @@
+age=int(input("enter the present age"))
+years_left=90-age
+days_left=365*years_left
+weeks_left=52*years_left
+months_left=12*years_left
+print(f"you have{days_left} days.{weeks_left} weeks and {months_left} months left")
